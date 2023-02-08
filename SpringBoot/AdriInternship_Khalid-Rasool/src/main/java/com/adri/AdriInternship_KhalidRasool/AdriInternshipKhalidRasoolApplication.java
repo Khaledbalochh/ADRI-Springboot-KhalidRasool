fa5d99e848bb244b16b04ce680133a77756bb441
Khaledbalochh <khaledelbrohi@gmail.com>
2023-02-08 21:54:53 +0300

    demo

---
+++ SpringBoot/AdriInternship_Khalid-Rasool/src/main/java/com/adri/AdriInternship_KhalidRasool/AdriInternshipKhalidRasoolApplication.java
@@ -0,0 +1,13 @@
+package com.adri.AdriInternship_KhalidRasool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdriInternshipKhalidRasoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdriInternshipKhalidRasoolApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/AdriInternship_Khalid-Rasool/src/test/java/com/adri/AdriInternship_KhalidRasool/AdriInternshipKhalidRasoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adri.AdriInternship_KhalidRasool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdriInternshipKhalidRasoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
