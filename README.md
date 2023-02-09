8d0eb72d3b2d5b6c07d71d3c9623d5057d8f810f
Khaledbalochh <khaledelbrohi@gmail.com>
2023-02-09 11:45:14 +0300

    demo

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ADRI-Springboot-KhalidRasool
-ADRI Internship
+# ADRI Internship program Solution
+Q1. In your own words, what is an API?
+Answer: an API is an interface which allows programs to communicate to eachother using requests.
+
+Q2. What's the difference between SQL and NoSQL databases?
+Answer: SQL databases store structured data with relationships in the form of rows and tables. on the other hand, NoSQL databases do not rely on relatationship schemas and use a different structure than rows and tables, much like a JSON string.
+
+Q3. What is Git and what is it used for?
+Answer: Git is a version control system which is used to track changes to the source code, it enables collaboration betwween various developers working on the same project.
+
+Q4. What constitutes clean code?
+Answer: Clean code means that the code should be easily readable by humans by using spaces and simple terms for variables. Apart from that, the code should be easy to mantain as well by ensuring everything is structured properly.
--- SpringBoot/AdriInternship_Khalid-Rasool/src/main/java/com/adri/AdriInternship_KhalidRasool/AppController.java
+++ SpringBoot/AdriInternship_Khalid-Rasool/src/main/java/com/adri/AdriInternship_KhalidRasool/AppController.java
@@ -1,57 +1,45 @@
 package com.adri.AdriInternship_KhalidRasool;
 
-
-
-
-
-
-
 import java.util.List;
-import java.util.NoSuchElementException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/app")
 public class AppController {
-    /**
-     * @return
-     */
+   
     @GetMapping("/hellomessage")
     public String HelloWorld(){
         return "Hello User!";
         }
 
         @Autowired
-        //private UserRepository UserRepository;
         private UserService service;
-        @GetMapping("/allusers")
+        @GetMapping("/all")
         public List<User> list(){
             return service.listAll();
         }
+
         @GetMapping("/{id}")
-public ResponseEntity<User> get(@PathVariable Integer id) {
-    try {
+        public ResponseEntity<User> get(@PathVariable Integer id) {
+   
         User user = service.get(id);
         return new ResponseEntity<User>(user, HttpStatus.OK);
-    } catch (NoSuchElementException e) {
-        return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-    }      
+        
 }
-@PostMapping(path="/add") 
-          public @ResponseBody String addNewUser (@RequestParam String name
+
+
+
+        @PostMapping(path="/add") 
+        public @ResponseBody User addNewUser (@RequestParam String name
               , @RequestParam String email,@RequestParam String information) {
             
             User n = new User();
@@ -59,33 +47,6 @@ public ResponseEntity<User> get(@PathVariable Integer id) {
             n.setEmail(email);
             n.setInformation(information);
             service.save(n);;
-            return "Saved";
-          }
-
-    /*
-           @GetMapping("/all")
-           public @ResponseBody Iterable<User> getAllUsers() {
-              return UserRepository.findAll();
-            }
-         
-         
-        
-            @PostMapping(path="/add") 
-          public @ResponseBody String addNewUser (@RequestParam String name
-              , @RequestParam String email,@RequestParam String information) {
-            
-            User n = new User();
-            n.setName(name);
-            n.setEmail(email);
-            n.setInformation(information);
-            UserRepository.save(n);
-            return "Saved";
-          }
-*/
-         /* @GetMapping("view/{id}")
-    public Optional<Customer> getCustomer(@PathVariable String user_name) {
-        return UserRepository.findByName(user_name);
-    }
-*/
-          
+            return n;
+          }   
 }
